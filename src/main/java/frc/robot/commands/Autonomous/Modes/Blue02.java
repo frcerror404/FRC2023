5f02ccd16e68d209c4df36e0ea960c94add5809f
Chad Krause <chad@chadkrause.com>
2023-04-06 12:03:38 -0400

    Removed unessesary Auto - Modes

--- src/main/java/frc/robot/commands/Autonomous/Modes/Blue02.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands.Autonomous.Modes;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Drivebase;
-
-public class Blue02 extends SequentialCommandGroup {
-    public Blue02(Drivebase m_drivebase) {
-    }
-}
--- src/main/java/frc/robot/commands/Autonomous/Modes/Red03.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.commands.Autonomous.Modes;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-public class Red03 extends SequentialCommandGroup {
-    
-}
